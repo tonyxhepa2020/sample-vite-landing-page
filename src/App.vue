8479da7608c3bbbc5fb30ac79a076a8a484494a2
Tony Xhepa <tonyxhepa2020@gmail.com>
2020-10-03 13:27:57 +0200

    example

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Navbar />
+  <Hero />
+  <Features />
+  <Testimonals />
+  <call-to-action></call-to-action>
+  <footer></footer>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import CallToAction from "./components/CallToAction.vue";
+import Features from "./components/Features.vue";
+import Hero from "./components/Hero.vue";
+import Testimonals from "./components/Testimonals.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    CallToAction,
+    Features,
+    Hero,
+    Testimonals,
+    Footer,
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="p-2 bg-blue-300">{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
